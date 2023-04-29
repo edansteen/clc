2dfe5a5729e9395a006b66d1236ec0e5c7296e2d
EdanSteen <edansteen@gmail.com>
2023-04-28 18:44:51 -0700

    worked on starting cutscene

--- scripts/StartingCutscene.gd
+++ scripts/StartingCutscene.gd
@@ -1,20 +1,25 @@
-extends Node
+extends Control
 
 var slideNum = 0
 var ready = false
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+onready var slides = $Slides.get_children()
 
 func _input(event):
 	if ready:
 		if event is InputEventMouseButton or event is InputEventKey:
+			$Slides/Continue.set_deferred("visible", false)
 			next_slide()
-			slideNum += 1
+
 func next_slide():
 	match(slideNum):
 		0:
 			var e = get_tree().change_scene("res://game/Main.tscn")
 			if e != OK:
 				print("Error! Couldn't load properly.")
+	slideNum += 1
+	$ShowContinue.start(4.0)
+
+func _on_ShowContinue_timeout():
+	ready = true
+	$CanvasLayer/Continue.set_deferred("visible", true)
